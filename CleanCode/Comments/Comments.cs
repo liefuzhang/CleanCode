d25f61adc56f215eb18abaab6b018ac9f1ba3b04
Liefu Zhang <liefu.zhang@aderant.com>
2018-01-26 13:53:25 +1300

    comment

--- CleanCode/Comments/Comments.cs
+++ CleanCode/Comments/Comments.cs
@@ -2,21 +2,16 @@
 using System.Collections.Generic;
 using System.Net.Mail;
 
-namespace CleanCode.Comments
-{
-    public class Comments
-    {
-        private int _pf;  // pay frequency
+namespace CleanCode.Comments {
+    public class Comments {
+        private int _payFrequency;
         private DbContext _dbContext;
 
-        public Comments()
-        {
+        public Comments() {
             _dbContext = new DbContext();
         }
 
-        // Returns list of customers in a country.
-        public List<Customer> GetCustomers(int countryCode)
-        {
+        public List<Customer> GetCustomers(int countryCode) {
             //TODO: We need to get rid of abcd once we revisit this method. Currently, it's 
             // creating a coupling betwen x and y and because of that we're not able to do 
             // xyz. 
@@ -24,46 +19,43 @@ namespace CleanCode.Comments
             throw new NotImplementedException();
         }
 
-        public void SubmitOrder(Order order)
-        {
-            // Save order to the database
+        public void SubmitOrder(Order order) {
+            SaveOrder(order);
+
+            SendEmailToCustomer(order);
+        }
+
+        private void SaveOrder(Order order) {
             _dbContext.Orders.Add(order);
             _dbContext.SaveChanges();
+        }
 
-            // Send an email to the customer
+        private static void SendEmailToCustomer(Order order) {
             var client = new SmtpClient();
             var message = new MailMessage("noreply@site.com", order.Customer.Email, "Your order was successfully placed.", ".");
             client.Send(message);
-
         }
     }
 
-    public class DbContext
-    {
+    public class DbContext {
         public DbSet<Order> Orders { get; set; }
 
-        public void SaveChanges()
-        {
+        public void SaveChanges() {
 
 
         }
     }
 
-    public class DbSet<T>
-    {
-        public void Add(Order order)
-        {
-
+    public class DbSet<T> {
+        public void Add(Order order) {
 
         }
     }
-    public class Order
-    {
+    public class Order {
         public Customer Customer { get; set; }
     }
 
-    public class Customer
-    {
+    public class Customer {
         public string Email { get; set; }
     }
 }
