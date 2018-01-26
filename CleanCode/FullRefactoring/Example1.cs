4603a5c4f905792b88d4cb55fc322ae835fc0633
liefu zhang <liefuzhangnz@gmail.com>
2018-01-26 22:01:42 +1300

    full refactoring

--- CleanCode/FullRefactoring/Example1.cs
+++ CleanCode/FullRefactoring/Example1.cs
@@ -5,139 +5,129 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Web.UI.WebControls;
 
-namespace Project.UserControls
-{
-    public class PostControl : System.Web.UI.UserControl
-    {
-        private PostDbContext DBContext;
+namespace CleanCode.FullRefactoring {
+    public class PostControl : System.Web.UI.UserControl {
+        private readonly PostRepository _postRepository;
+        private readonly PostValidator _validator;
+        private Label PostBody { get; set; }
+        private Label PostTitle { get; set; }
+        private int? PostId { get; set; }
 
+        public PostControl() {
+            _postRepository = new PostRepository();
+            _validator = new PostValidator();
+        }
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            DBContext = new PostDbContext();
-
-            if (Page.IsPostBack)
-            {
-                PostValidator validator = new PostValidator();
-                Post entity = new Post()
-                {
-                    // Map form fields to entity properties
-                    Id = Convert.ToInt32(PostId.Value),
-                    Title = PostTitle.Text.Trim(),
-                    Body = PostBody.Text.Trim()
-                };
-                ValidationResult results = validator.Validate(entity);
-
-                if (results.IsValid)
-                {
-                    // Save to the database and continue to the next page
-                    DBContext.Posts.Add(entity);
-                    DBContext.SaveChanges();
-                }
-                else
-                {
-                    BulletedList summary = (BulletedList)FindControl("ErrorSummary");
-
-                    // Display errors to the user
-                    foreach (var failure in results.Errors)
-                    {
-                        Label errorMessage = FindControl(failure.PropertyName + "Error") as Label;
-
-                        if (errorMessage == null)
-                        {
-                            summary.Items.Add(new ListItem(failure.ErrorMessage));
-                        }
-                        else
-                        {
-                            errorMessage.Text = failure.ErrorMessage;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                // Display form
-                Post entity = DBContext.Posts.SingleOrDefault(p => p.Id == Convert.ToInt32(Request.QueryString["id"]));
-                PostBody.Text = entity.Body;
-                PostTitle.Text = entity.Title;
-
+        protected void Page_Load(object sender, EventArgs e) {
+            if (Page.IsPostBack) {
+                TrySavePost();
+            } else {
+                DisplayPost();
             }
         }
 
-        public Label PostBody { get; set; }
+        private void TrySavePost() {
+            var post = GetPost();
+            var results = _validator.Validate(post);
 
-        public Label PostTitle { get; set; }
+            if (results.IsValid) {
+                _postRepository.SavePost(post);
+            } else {
+                DisplayErrors(results);
+            }
+        }
 
-        public int? PostId { get; set; }
+        private Post GetPost() {
+            return new Post() {
+                Id = Convert.ToInt32(PostId.Value),
+                Title = PostTitle.Text.Trim(),
+                Body = PostBody.Text.Trim()
+            };
+        }
+
+        private void DisplayErrors(ValidationResult results) {
+            var summary = GetErrorSummaryControl();
+
+            foreach (var error in results.Errors) {
+                var label = GetErrorLabelControl(error);
+
+                if (label == null) {
+                    summary.Items.Add(new ListItem(error.ErrorMessage));
+                } else {
+                    label.Text = error.ErrorMessage;
+                }
+            }
+        }
+
+        private Label GetErrorLabelControl(ValidationError error) {
+            return FindControl(error.PropertyName + "Error") as Label;
+        }
+
+        private BulletedList GetErrorSummaryControl() {
+            return (BulletedList)FindControl("ErrorSummary");
+        }
+
+        private void DisplayPost() {
+            var postId = Convert.ToInt32(Request.QueryString["id"]);
+            var post = _postRepository.GetPost(postId);
+            PostBody.Text = post.Body;
+            PostTitle.Text = post.Title;
+        }
     }
 
     #region helpers
 
-    public class ValidationResult
-    {
+    public class ValidationResult {
         public bool IsValid { get; set; }
         public IEnumerable<ValidationError> Errors { get; set; }
     }
 
-    public class ValidationError
-    {
+    public class ValidationError {
         public string PropertyName { get; set; }
         public string ErrorMessage { get; set; }
     }
 
-    public class Post
-    {
+    public class Post {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
     }
 
-    public class PostValidator
-    {
-        public ValidationResult Validate(Post entity)
-        {
+    public class PostValidator {
+        public ValidationResult Validate(Post entity) {
             throw new NotImplementedException();
         }
     }
 
-    public class DbSet<T> : IQueryable<T>
-    {
-        public void Add(T entity)
-        {
+    public class DbSet<T> : IQueryable<T> {
+        public void Add(T entity) {
         }
 
-        public IEnumerator<T> GetEnumerator()
-        {
+        public IEnumerator<T> GetEnumerator() {
             throw new NotImplementedException();
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
+        IEnumerator IEnumerable.GetEnumerator() {
             return GetEnumerator();
         }
 
-        public Expression Expression
-        {
+        public Expression Expression {
             get { throw new NotImplementedException(); }
         }
 
-        public Type ElementType
-        {
+        public Type ElementType {
             get { throw new NotImplementedException(); }
         }
 
-        public IQueryProvider Provider
-        {
+        public IQueryProvider Provider {
             get { throw new NotImplementedException(); }
         }
     }
 
-    public class PostDbContext
-    {
+    public class PostDbContext {
         public DbSet<Post> Posts { get; set; }
 
-        public void SaveChanges()
-        {
+        public void SaveChanges() {
         }
     }
     #endregion
--- CleanCode/FullRefactoring/PostRepository.cs
+++ CleanCode/FullRefactoring/PostRepository.cs
@@ -1,23 +1,18 @@
 using System.Linq;
 
-namespace CleanCode.FullRefactoring
-{
-    public class PostRepository
-    {
+namespace CleanCode.FullRefactoring {
+    public class PostRepository {
         private readonly PostDbContext _dbContext;
 
-        public PostRepository()
-        {
+        public PostRepository() {
             _dbContext = new PostDbContext();
         }
 
-        public Post GetPost(int postId)
-        {
+        public Post GetPost(int postId) {
             return _dbContext.Posts.SingleOrDefault(p => p.Id == postId);
         }
 
-        public void SavePost(Post post)
-        {
+        public void SavePost(Post post) {
             _dbContext.Posts.Add(post);
             _dbContext.SaveChanges();
         }
